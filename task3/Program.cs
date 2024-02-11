0eaa841ab3c7a9fe9742c44444d1d51d2030c5a0
Andrey Krasnov <tiralmaz@mail.ru>
2024-02-11 22:30:06 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+
+int[] inputArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 301); // [100, 999]
+    return array;
+}
+
+void expandArr (int[] array) {
+    Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+    int tmp;
+    for (int i = 0; i < array.Length / 2; i++){
+        tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
+    }
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+}
+
+expandArr(inputArray(array));
+
