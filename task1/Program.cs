a30e07bbb0418b59fef8ab823323f6b2f8cebf6f
Andrey Krasnov <tiralmaz@mail.ru>
2024-02-11 22:19:18 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿void questNumber () {
+    while(true) {
+        Console.WriteLine("Введите целое число: ");
+        string str = Console.ReadLine()!;
+        int number;
+        int num = 0;
+
+        if(str == "q") break;
+        
+        if(int.TryParse(str, out number)){
+            for(int i = 0; i < str.Length; i++){
+                num += str[i] - '0';
+                Console.WriteLine(num);
+            }
+            if(num % 2 == 0) break;
+        }
+    }
+}
+
+questNumber(); 
+
